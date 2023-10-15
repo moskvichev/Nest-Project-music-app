6d70b0f1f44b74fa96e0a2e4dabeee17aa693e0b
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-10-15 21:13:34 +0400

    layout

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,18 @@
-import Navbar from '@/components/Navbar';
+import MainLayout from '@/layouts/MainLayout';
 import Button from '@mui/material/Button';
 import React from 'react';
 
 const Index = () => {
   return (
     <>
-      <Navbar />
-      <div className="center">
-        <h1>Welcome to main page.</h1>
-        <h3>Here the best music tracks!</h3>
-        <Button>Push the button</Button>
-      </div>
+      <MainLayout>
+        <div className="center">
+          <h1>Welcome to main page.</h1>
+          <h3>Here the best music tracks!</h3>
+          <Button>Push the button</Button>
+        </div>
+      </MainLayout>
+
       <style jsx>
         {`
           .center {
---
+++ client/pages/tracks/index.tsx
@@ -0,0 +1,14 @@
+import MainLayout from '@/layouts/MainLayout';
+import React from 'react';
+
+const Index = () => {
+  return (
+    <>
+      <MainLayout>
+        <div>Список треков</div>
+      </MainLayout>
+    </>
+  );
+};
+
+export default Index;
