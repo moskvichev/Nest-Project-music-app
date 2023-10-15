17fbb4464ab00af8d9e1b48d3c5865bfee337551
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-10-15 16:17:42 +0400

    added navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,28 @@
+import Navbar from '@/components/Navbar';
 import Button from '@mui/material/Button';
 import React from 'react';
 
 const Index = () => {
   return (
-    <div>
-      Главная страница
-      <Button>Push the button</Button>
-    </div>
+    <>
+      <Navbar />
+      <div className="center">
+        <h1>Welcome to main page.</h1>
+        <h3>Here the best music tracks!</h3>
+        <Button>Push the button</Button>
+      </div>
+      <style jsx>
+        {`
+          .center {
+            margin-top: 150px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
