c165ba53ff4057adc503df24cf5dee67c0696b1e
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-10-19 23:41:22 +0400

    new page

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,11 +1,23 @@
 import MainLayout from '@/layouts/MainLayout';
+import { Box, Button, Card, Grid } from '@mui/material';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Index = () => {
+  const router = useRouter();
   return (
     <>
       <MainLayout>
-        <div>Список треков</div>
+        <Grid container justifyContent="center">
+          <Card style={{ width: 900 }}>
+            <Box p={3}>
+              <Grid container justifyContent="space-between">
+                <h1>Список треков</h1>
+                <Button onClick={() => router.push('/tracks/create')}>Загрузить</Button>
+              </Grid>
+            </Box>
+          </Card>
+        </Grid>
       </MainLayout>
     </>
   );
