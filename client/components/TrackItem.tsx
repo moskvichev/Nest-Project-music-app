437cbeea9d91e6cfe8063eaf637a812e4b04cf9c
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-10-24 23:53:26 +0400

    fix front

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -15,7 +15,7 @@ const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
   return (
     <Card className={styles.track} onClick={() => router.push('/tracks/' + track._id)}>
       <IconButton>{active ? <Pause /> : <PlayArrow />}</IconButton>
-      <img width={71} height={70} src={track.picture} />
+      <img width={70} height={70} src={track.picture} />
       <Grid container direction="column" style={{ width: 200, margin: '0 20px' }}>
         <div>{track.name}</div>
         <div style={{ fontSize: 12, color: 'grey' }}>{track.artist}</div>
