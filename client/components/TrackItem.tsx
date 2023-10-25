7901df6c74391f84f442b5978cec8b8acb0339b4
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-10-25 23:54:28 +0400

    new track

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -14,14 +14,16 @@ const TrackItem: React.FC<TrackItemProps> = ({ track, active = false }) => {
   const router = useRouter();
   return (
     <Card className={styles.track} onClick={() => router.push('/tracks/' + track._id)}>
-      <IconButton>{active ? <Pause /> : <PlayArrow />}</IconButton>
+      <IconButton onClick={(e) => e.stopPropagation()}>
+        {active ? <Pause /> : <PlayArrow />}
+      </IconButton>
       <img width={70} height={70} src={track.picture} />
       <Grid container direction="column" style={{ width: 200, margin: '0 20px' }}>
         <div>{track.name}</div>
         <div style={{ fontSize: 12, color: 'grey' }}>{track.artist}</div>
       </Grid>
       {active && <div>02:42 / 03:22</div>}
-      <IconButton style={{ marginLeft: 'auto' }}>
+      <IconButton onClick={(e) => e.stopPropagation()} style={{ marginLeft: 'auto' }}>
         <Delete />
       </IconButton>
     </Card>
