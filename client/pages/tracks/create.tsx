16ff4931765185cb9d5d10738824a0d586cfb628
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-11-20 23:45:18 +0400

    added buttons next prev

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,6 +1,6 @@
 import StepWrapper from '@/components/StepsWrapper';
 import MainLayout from '@/layouts/MainLayout';
-import { Card, Grid } from '@mui/material';
+import { Button, Card, Grid } from '@mui/material';
 import React from 'react';
 
 const Create = () => {
@@ -9,6 +9,10 @@ const Create = () => {
       <StepWrapper activeStep={1}>
         <h1>Загрузка трека</h1>
       </StepWrapper>
+      <Grid>
+        <Button>Назад</Button>
+        <Button>Далее</Button>
+      </Grid>
     </MainLayout>
   );
 };
