d4dc756e751c308fa8545a2ea5cc739cd57e787b
moskvichev evgeniy <moskvichev_e@bk.ru>
2023-11-20 23:58:26 +0400

    added buttons next prev func

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,17 +1,24 @@
 import StepWrapper from '@/components/StepsWrapper';
 import MainLayout from '@/layouts/MainLayout';
 import { Button, Card, Grid } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Create = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const next = () => {
+    setActiveStep((prev) => prev + 1);
+  };
+  const back = () => {
+    setActiveStep((prev) => prev - 1);
+  };
   return (
     <MainLayout>
-      <StepWrapper activeStep={1}>
+      <StepWrapper activeStep={activeStep}>
         <h1>Загрузка трека</h1>
       </StepWrapper>
-      <Grid>
-        <Button>Назад</Button>
-        <Button>Далее</Button>
+      <Grid container justifyContent="space-between">
+        <Button onClick={back}>Назад</Button>
+        <Button onClick={next}>Далее</Button>
       </Grid>
     </MainLayout>
   );
